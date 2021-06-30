7514372232855418c40d3d2d2cb438e860c490bc
landroide13 <lazaro02@hotmail.com>
2021-07-01 10:41:58 +1200

    password generator

--- Password.cs
+++ Password.cs
@@ -18,6 +18,25 @@ namespace TicketTracker
       this.timestamp = "";
     }
 
+    public Password(string staffId, int ticketNumber, string timestamp)
+    {
+      this.staffId = staffId;
+      this.ticketNumber = ticketNumber;
+      this.timestamp = DateTime.Now.ToString();
+    }
+
+    public void genNewPassword()
+    {
+      string pass1 = staffId.Substring(0,2);
+      string pass2 = ticketNumber.ToString("X");
+
+      string timeSt = timestamp.Substring(0, 2) + timestamp[3];
+      int numberT = Convert.ToInt32(timeSt);
+
+      string pass3Hex = numberT.ToString("X");
+
+    }
+
   }
 
 
